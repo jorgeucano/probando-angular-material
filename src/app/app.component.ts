28247db0d7bbe7232e5a6a72fad34c1d30265ef8
jorgeucano <jorgeucano@gmail.com>
2016-12-23 12:27:04 -0300

    first example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,13 @@ export class AppComponent {
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
+
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
   
 
 }
